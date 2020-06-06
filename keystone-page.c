e8dc3b151d1eeec16c119a68f8a8b5d3ea8e64f5
Alex Thomas <alexthomas@berkeley.edu>
2020-06-06 09:38:13 +0000

    Fix formatting

--- keystone-page.c
+++ keystone-page.c
@@ -40,15 +40,15 @@ int epm_init(struct epm* epm, unsigned int min_pages)
   count = 0x1 << order;
 
   /* prevent kernel from complaining about an invalid argument */
-  if (order <= MAX_ORDER) 
-   epm_vaddr = (vaddr_t) __get_free_pages(GFP_HIGHUSER, order);
+  if (order <= MAX_ORDER)
+    epm_vaddr = (vaddr_t) __get_free_pages(GFP_HIGHUSER, order);
 
 #ifdef CONFIG_CMA
   /* If buddy allocator fails, we fall back to the CMA */
   if (!epm_vaddr) {
     epm->is_cma = 1;
     count = min_pages;
-  
+
     epm_vaddr = (vaddr_t) dma_alloc_coherent(keystone_dev.this_device,
       count << PAGE_SHIFT,
       &device_phys_addr,
